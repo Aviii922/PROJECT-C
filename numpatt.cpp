f664e0ec3a1cd9c7dedfb747731768860211568e
Avinash Singh <avinashsingh4866@gmail.com>
2026-06-06 01:14:19 +0530

    c++

--- numpatt.cpp
+++ numpatt.cpp
@@ -4,10 +4,10 @@ int main(){
     int n=5;
     //cout<<"Enter n ";
     //cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<i+1;j++){
+    for(int i=1;i<n;i++){
+        for(int j=1;j<i+1;j++){
             
-            cout<<"*<<" ";
+            cout<<i<<" ";
         
         }
         
